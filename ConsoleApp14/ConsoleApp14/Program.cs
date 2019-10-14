a57c9a313a34a0b1a40b5b93e0223e333b7fd263
matva22 <56400847+matva22@users.noreply.github.com>
2019-10-14 14:06:05 +0400

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                double a, b, c, d;
+            
+            Console.WriteLine("A=");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("B=");
+            b = Convert.ToDouble(Console.ReadLine());
+            c = a + b;
+            Console.WriteLine(c);
+            
+                Console.Read();
+            
+        }
+    }
+}
